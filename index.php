4d80d33df9584e779a253c1f084ff0423c786865
Maxime Tournier <mxm.tournier@gmail.com>
2024-04-09 19:19:22 +0200

    final code

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+require "module/form.phtml";
+
+if (isset($_FILES['bashfile']) && isset($_POST['name'])) {
+
+    // Vérification si c'est une image
+    $image_info = getimagesize($_FILES["bashfile"]["tmp_name"]);
+
+        // Vérification de la taille du fichier
+        $max_file_size = 9 * 1024 * 1024; // 9 Mo
+        if ($_FILES["bashfile"]["size"] <= $max_file_size) {
+
+            // Dossier de destination
+            $upload_dir = 'C:\Users\mxmto\Developpement\LocalHost\www\UploadEvaluationLinux\uploads/';
+            // Nouveau nom de fichier pour éviter les collisions
+            $upload_file = $upload_dir . '_' . $_POST['name'] . '_bash_history-' . uniqid() . '.log';
+
+            // Déplacer le fichier téléchargé vers le dossier de destination
+            if (move_uploaded_file($_FILES["bashfile"]["tmp_name"], $upload_file)) {
+
+                resultPicture(1, "Votre Fichier à bien été envoyé.");
+
+
+            } else {
+                resultPicture(2, "Une erreur s'est produite lors du déplacement du fichier vers le dossier de destination.");
+            }
+
+        } else {
+            resultPicture(2, "La taille du fichier dépasse la limite autorisée de 9 Mo.");
+        }
+
+
+}
+
+function resultPicture($why, $msg)
+{
+
+    if ($why == 1) {
+
+        header("location: ?p=2&true=" . $msg);
+
+    }
+
+    if ($why == 2) {
+
+        header("location: ?p=2&err=" . $msg);
+    }
+
+}
